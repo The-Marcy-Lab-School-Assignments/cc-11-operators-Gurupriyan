73399b679558008d1146ddb9183157e3054534b2
Gurupriyan <gurupriyan98@gmail.com>
2021-11-02 14:11:40 +0000

    Code challenge completed

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 //1
-function areBothSame(){
-  
+function areBothSame(a, b){
+    return (a == b) ? true : false;
 }
 
 //2
-function areBothEqual(){
-  
+function areBothEqual(a, b){
+    return (a === b) ? true : false;
 }
 
 //3
-function areAllFourEqual(){
-  
+function areAllFourEqual(a, b, c, d){
+    return (a === b) ? ((b === c) ? ((c === d) ? true : false) : false) : false;
 }
 
 
 //4
-function areAllFourSame(){
-  
+function areAllFourSame(a, b, c, d){
+    return (a == b) ? ((b == c) ? ((c == d) ? true : false) : false) : false;
 }
 
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual(a, b, c, d){
+    if (a == b && b == c && c == d){
+        if (a === b && b === c && c === d){
+            return "equal";
+        }else return "same";
+    }else return null;
 }
 
-
+console.log(areAllFourSameOrEqual())
